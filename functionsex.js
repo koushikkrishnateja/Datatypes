fd261e77e0a4f3f43a08659999977df97f8c5a1f
koushikkrishnateja <2211cs010519@mallareddyuniversity.ac.in>
2025-01-09 10:27:36 +0530

    msg

--- functionsex.js
+++ functionsex.js
@@ -22,9 +22,26 @@ add(8,9);
 
 
 // function expression 
-let x = function(){
-    console.log("anonymous is not executing but executing with variable");
-}
+//let x = function(){
+  //  console.log("anonymous is not executing but executing with variable");
+//}
 
 //console.log(x);
-x();
+//x();
+
+//arrow function
+//let x =(a,b)=>console.log(a,b); {
+    
+//}
+//x();
+// impicit returns and explicit returns.
+function test1(a,b){
+    console.log("hello world");
+    return a+b;
+    console.log("hi but this is not working");
+}
+console.log(test1(5,5));
+let x =(a,b)=>{return a*b};
+console.log(x(5,5));
+let y = (a,b) => a+b;
+console.log(y(100,200));
