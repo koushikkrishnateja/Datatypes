69c803655d2c943687cb93a3dabf61b3e5afa8c1
koushikkrishnateja <2211cs010519@mallareddyuniversity.ac.in>
2025-01-09 09:52:59 +0530

    My commit

---
+++ functionsex.js
@@ -0,0 +1,30 @@
+//function demo(){
+//    console.log("function is excuting.....");
+//}
+//demo();
+//demo();
+
+
+//!function with parameters and argus
+//let a = 10 // parameter,10=argus
+5,6
+8,9 
+3,4
+function add(a,b){
+    console.log(a+b);
+
+}
+add(5,6);
+add(8,9);
+// !anonymous function
+
+
+
+
+// function expression 
+let x = function(){
+    console.log("anonymous is not executing but executing with variable");
+}
+
+//console.log(x);
+x();
