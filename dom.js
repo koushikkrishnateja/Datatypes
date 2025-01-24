5e6e941d639e7a4754f6ef13a36c68536362f294
koushikkrishnateja <2211cs010519@mallareddyuniversity.ac.in>
2025-01-24 11:47:15 +0530

    Your commit message

---
+++ dom.js
@@ -0,0 +1,32 @@
+// let ele = document.getElementsByClassName("test");
+// ele[0].computedStyleMap.backgroundColor="dodgerblue"
+// console.log(ele);
+// console.log(Array.isArray(ele));
+
+
+// //spred operator : this operator is used ot take out each and veery value from
+
+// let x  = [...ele];
+// console.log(x,Array.isArray(x));
+// x.map(element=>{
+//     element.style.backgroundColor="tomato";
+//     element.style.color="white";
+//     element.style.textTransform="uppercase";
+// })
+// ele[0].style.backgroundColor="violet";
+// ele[1].style.backgroundColor="indigo";
+// ele[2].style.backgroundColor="blue";
+// ele[3].style.backgroundColor="green";
+// ele[4].style.backgroundColor="yellow";
+// ele[5].style.backgroundColor="orange";
+// ele[6].style.backgroundColor="red";
+
+
+let ele =  document.querySelector("#demo");
+console.log(ele);
+// let ele =  document.querySelector(".test");
+// console.log(ele);
+
+
+// let ele =  document.querySelectorAll(".test");
+// console.log(ele);
