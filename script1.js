3e854859830f1dd16428d57d84f3202ea97d4856
koushikkrishnateja <2211cs010519@mallareddyuniversity.ac.in>
2025-01-30 11:34:55 +0530

    commit your message

---
+++ script1.js
@@ -0,0 +1,7 @@
+let msg = "hello,good morning!!!";
+function greet(){
+    return msg+"i am coding now";
+}
+let marks = 90;
+
+export default greet;marks;
---
+++ script2.js
@@ -0,0 +1,12 @@
+// import {msg} from "./script1.js";
+// import {greet} from "./script1.js";
+  
+// console.log(msg);
+// let abc = greet();
+// console.log(abc); 
+
+
+import mru from "./script1.js"
+import ms from "./script1.js"
+console.log(mru());
+console.log(ms);
